a0028494ca22684241c77dcee397c74306dfdd04
martin benoit <mrb4157a@ens.fsi>
2022-11-15 16:31:18 +0100

    les samourais

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -16,5 +16,8 @@ public class HistoireTP5 {
 	marco.listerConnaissance();
 	roro.listerConnaissance();
 	yaku.listerConnaissance();
+	Samourai akimoto = new Samourai("Miyamoto","Akimoto", "saké", 80);
+	akimoto.faireConnaissanceAvec(marco);
+	akimoto.listerConnaissance();
 	}
 }
